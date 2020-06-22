430a64d008854506f743ba0a7539b82d06871e2b
Nilesh Kumar Gupta <f20180233@goa.bits-pilani.ac.in>
2020-06-22 13:01:29 +0530

    testing db function

---
+++ db.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+// compile with g++ -o db db.cpp `pkg-config --libs --cflags libmongocxx`
+
+int main(int, char **)
+{
+  mongocxx::instance inst{};
+  mongocxx::client conn{mongocxx::uri{}};
+
+  bsoncxx::builder::stream::document document{};
+
+  auto collection = conn["testdb"]["testcollection"];
+  document << "hello"
+           << "world";
+
+  collection.insert_one(document.view());
+  auto cursor = collection.find({});
+
+  for (auto &&doc : cursor)
+  {
+    std::cout << bsoncxx::to_json(doc) << std::endl;
+  }
+}
